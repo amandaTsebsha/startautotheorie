cac2a0f836fcd3094dd0d703940a18bcc8eca39d
amynollz153@gmail.com <amynollz153@gmail.com>
2025-01-19 06:58:09 +0200

    Updated Settings

--- start_auto_theorie/settings.py
+++ start_auto_theorie/settings.py
@@ -9,7 +9,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['startautotheorie.onrender.com', ' http://127.0.0.1:8000', 'localhost'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['startautotheorie.onrender.com', ' http://127.0.0.1:8000/', 'localhost'])
 
 DATABASES = {
     'default': env.db('DATABASE_URL', default=f'sqlite:///{BASE_DIR / "startautotheorie.sqlite"}')
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'apps.practice',
     'apps.users',
     'whitenoise.runserver_nostatic',  # Optional: Makes WhiteNoise work with runserver
